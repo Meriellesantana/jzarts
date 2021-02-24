470a5d797f4e70e67a985e1b2efa156f8b04307c
Meire <mgebombom@hotmail.com>
2021-02-24 17:01:34 -0300

    finalizado

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-$('.btm').on("click",function(e){ 
-     e.preventDefault();
-    $('.sub2').slideToggle('fast',function(){
-      if(!$(this).is(':hidden')) {
-        $('.btm .botao').attr('src',"imgmobile/botao-menu-aberto.png" );
+  $('.btm').on("click",function(e){ 
+       e.preventDefault();
+      $('#sub2').slideToggle('fast',function(){
+        if(!$(this).is(':hidden')) {
+          $('.btm .botao').attr('src',"imgmobile/botao-menu-aberto.png" );
+          }
+        else {
+          $('.btm .botao').attr('src',"imgmobile/botao-menu.png" );     
         }
-      else {
-        $('.btm .botao').attr('src',"imgmobile/botao-menu.png" );     
-      }
-    });
-});
-
-});
+      });
+  });
+  
+  });
